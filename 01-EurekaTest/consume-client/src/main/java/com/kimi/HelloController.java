8ed313610a816d9c477eaafa96a110c7d88d603a
CZero <hz2z0421@163.com>
2017-12-27 10:40:32 +0800

    init

---
+++ 01-EurekaTest/consume-client/src/main/java/com/kimi/HelloController.java
@@ -0,0 +1,21 @@
+package com.kimi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by geek on 2017/12/26.
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return restTemplate.getForEntity("http://SERVICE-HELLO/hello", String.class).getBody();
+    }
+}
