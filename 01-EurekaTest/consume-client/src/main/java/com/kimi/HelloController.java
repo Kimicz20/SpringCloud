1d12f842550c39f8b82cfd029de83959dc02b151
CZero <hz2z0421@163.com>
2017-12-28 23:24:34 +0800

    adj

--- 01-EurekaTest/consume-client/src/main/java/com/kimi/HelloController.java
+++ 01-EurekaTest/consume-client/src/main/java/com/kimi/HelloController.java
@@ -1,13 +1,10 @@
 package com.kimi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by geek on 2017/12/26.
@@ -15,15 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    protected Logger logger = LoggerFactory.getLogger(getClass());
-
     @Autowired
-    private DiscoveryClient client;
+    private RestTemplate restTemplate;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod.GET)
     public String hello() {
-        ServiceInstance instance = client.getInstances("SERVICE-HELLO").get(0);
-        logger.info("/hello, host:"+instance.getHost()+",service_id:"+instance.getServiceId());
-        return "Hello world!";
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello",String.class).getBody();
     }
 }
